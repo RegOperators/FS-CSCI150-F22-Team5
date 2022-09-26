adff0bcd081fbaa317384a7189b7868302179376
Luis Gutierrez <byfractl@gmail.com>
2022-09-25 17:52:02 -0700

    Change the dark mode colors and begin working on the busy periods UI

--- app/src/App.js
+++ app/src/App.js
@@ -63,23 +63,63 @@ function App() {
   }
   
   return (
-    <div className="dark:bg-gray-900 dark:text-white font-['Manrope']">
+    <div className="dark:bg-[#0d1117] dark:text-white font-['Manrope']">
       <header className="px-6 py-4">
         <img src={logo} className="w-16 h-16" alt="logo" />
       </header>
-      <div className="py-32 container mx-auto px-6 text-center">
-        <h1 className="text-4xl sm:text-7xl font-extrabold mb-16">Create the perfect <span className="text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-orange-400">class schedule</span> in a matter of seconds!</h1>
+      <div className="py-40 container mx-auto px-6 text-center">
+        <h1 className="text-5xl sm:text-8xl font-extrabold mb-16">Create the perfect <span className="text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-orange-400">class schedule</span> in a matter of seconds!</h1>
         <p className="text-lg mb-16">All you have to do is specify the classes you'd like to take, along with some preferences, and we'll take care of the rest.</p>
+        <div className="grid grid-cols-1 sm:grid-cols-10 gap-8 mb-16">
+          <div className="col-span-6">
+            <div className="relative">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute top-2.5 left-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              </svg>
+              <input className="bg-gray-100 dark:bg-[#161b22] pl-14 pr-4 py-2.5 rounded-md w-full mb-8" />
+            </div>
+            <ul className="">
+              {courses.map((course, index) => (
+                <li className="bg-gray-100 dark:bg-[#161b22] p-16 rounded-md flex flex-col sm:flex-row mb-4 last:mb-0" key={index}>
+                  <div>CSCI 117 - Structures of Programming Languages</div>
+                  <div>3</div>
+                  <button onClick={() => removeCourse(index)}>Remove</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <ul className="col-span-4">
+            {courses.map((course, index) => (
+              <li className="bg-gray-100 dark:bg-[#161b22] p-16 rounded-md flex flex-col sm:flex-row mb-4 last:mb-0" key={index}>
+                <div>CSCI 117 - Structures of Programming Languages</div>
+                <div>3</div>
+                <button onClick={() => removeCourse(index)}>Remove</button>
+              </li>
+            ))}
+          </ul>
+        </div>
         <ul className="mb-6">
-          {courses.map((course, index) => (
-            <li className="mb-4 last:mb-0" key={index}>
-              <input className="bg-gray-100 dark:bg-gray-800 px-4 py-2 rounded mr-4" value={course.courseId} onChange={(event) => updateCourse(index, event)} />
-              <button onClick={() => removeCourse(index)}>Remove</button>
+          {busyPeriods.map((busyPeriod, index) => (
+            <li className="grid grid-cols-2 sm:grid-cols-10 gap-4 mb-4 last:mb-0" key={index}>
+              <div className="col-span-2">
+                <label className="block mb-2">Name</label>
+                <input className="bg-gray-100 dark:bg-[#161b22] px-4 py-2.5 rounded-md w-full" value={busyPeriod.name} onChange={(event) => updateBusyPeriod(index, event)} />
+              </div>
+              <div>
+                <label className="block mb-2">Start Time</label>
+                <input className="bg-gray-100 dark:bg-[#161b22] px-4 py-2.5 rounded-md w-full" type="time" value={busyPeriod.startTime} onChange={(event) => updateBusyPeriod(index, event)} />
+              </div>
+              <div>
+                <label className="block mb-2">End Time</label>
+                <input className="bg-gray-100 dark:bg-[#161b22] px-4 py-2.5 rounded-md w-full" type="time" value={busyPeriod.endTime} onChange={(event) => updateBusyPeriod(index, event)} />
+              </div>
+              <input className="" type="checkbox" />
+              <button onClick={() => removeBusyPeriod(index)}>Remove</button>
             </li>
           ))}
         </ul>
-        <button className="bg-gray-100 dark:bg-gray-800 px-4 py-2 rounded" onClick={addCourse}>Add Course</button>
-        <button className="bg-gradient-to-br from-red-400 to-orange-400 px-4 py-2 rounded" onClick={fetchSchedules}>Generate Schedules</button>
+        <button className="bg-gray-100 dark:bg-[#161b22] px-4 py-2.5 rounded-md" onClick={addCourse}>Add Course</button>
+        <button className="bg-gradient-to-br from-red-400 to-orange-400 px-4 py-2.5 rounded-md" onClick={fetchSchedules}>Generate Schedules</button>
       </div>
       <table>
         <tr>
