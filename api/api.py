92bce225c0b6e8901ebf818c5f267ea6276cc688
garciacce23 <62458331+garciacce23@users.noreply.github.com>
2022-10-06 15:47:06 -0700

    Uncommented get_course_info() app route and added comments explaining function

--- api/api.py
+++ api/api.py
@@ -10,28 +10,36 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-
-# @app.route('/courses')
-def get_course_info():
-    # return JSON object
+@app.route("/courses")
+def get_course_info():  # convert course info xml to json obj indexed by courseID
+    # scrape xml from site
     xml = requests.get('https://www.fresnostate.edu/catalog-xml/frcourses.xml').text
     soup = BeautifulSoup(xml, 'xml')
 
+    # create empty 'master' dictionary to hold json entries
     catalogue = {}
 
-    courses = list()
+    # iterate through all courses in the xml from site
     for course in soup.find_all('COURSE'):
+        # create empty dictionary to hold current course json entry info
         entry = {}
+
+        # iterate through each 'info field' in the current course
         for contents in course.contents[1:-1]:
-            name = contents.name
-            text = contents.text
-            entry[name] = text
-            # print(f"{name}: {text}")
-        catalogue[entry["CRSE_ID"]] = entry
+            name = contents.name  # save the name of the current 'info field'
+            text = contents.text  # save the text contained by the current 'info field'
 
-    json_object = json.dumps(catalogue, indent=4, sort_keys=True)
+            entry[name] = text # add current info field to current course json entry dict
 
+        # Add current course json entry to 'master' dictionary
+        catalogue[entry["CRSE_ID"]] = entry  # current entry's course ID is used for key name
+
+    # Create json object from 'master' dictionary of all courses
+    json_object = json.dumps(catalogue, indent=4, sort_keys=True)  # note that sorting is enabled
+
+    # Return final json
     return json_object
+
 
 @app.route('/')
 def get_valid_schedules():
@@ -50,7 +58,8 @@ def get_valid_schedules():
         'languageCd': languageCd,
         'fr_guest_token': fr_guest_token,
     }
-    response = session.post('https://cmsweb.fresnostate.edu/psc/CFRPRD/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL', data=data)
+    response = session.post(
+        'https://cmsweb.fresnostate.edu/psc/CFRPRD/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL', data=data)
     sessionId = session.cookies['CFRPRD-PSJSESSIONID']
     icStateNum = 1
     classes = []
@@ -62,14 +71,16 @@ def get_valid_schedules():
         data = {
             'ICStateNum': icStateNum,
             'ICAction': 'CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH',
-            #'CLASS_SRCH_WRK2_STRM$35$': '2227',
+            # 'CLASS_SRCH_WRK2_STRM$35$': '2227',
             'SSR_CLSRCH_WRK_SUBJECT$0': className.split()[0],
             'SSR_CLSRCH_WRK_SSR_EXACT_MATCH1$1': 'E',
             'SSR_CLSRCH_WRK_CATALOG_NBR$1': className.split()[1],
-            #'SSR_CLSRCH_WRK_SSR_OPEN_ONLY$chk$4': 'N',
+            # 'SSR_CLSRCH_WRK_SSR_OPEN_ONLY$chk$4': 'N',
         }
 
-        response = requests.post('https://cmsweb.fresnostate.edu/psc/CFRPRD/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL', cookies=cookies, data=data)
+        response = requests.post(
+            'https://cmsweb.fresnostate.edu/psc/CFRPRD/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL',
+            cookies=cookies, data=data)
         soup = BeautifulSoup(response.text, 'html5lib')
         okButton = soup.find(id='#ICSave')
         if okButton:
@@ -82,21 +93,24 @@ def get_valid_schedules():
                 'ICAction': '#ICSave',
             }
 
-            response = requests.post('https://cmsweb.fresnostate.edu/psc/CFRPRD/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL', cookies=cookies, data=data)
+            response = requests.post(
+                'https://cmsweb.fresnostate.edu/psc/CFRPRD/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL',
+                cookies=cookies, data=data)
             soup = BeautifulSoup(response.text, 'html5lib')
-        
+
         errorText = soup.find(id='DERIVED_CLSMSG_ERROR_TEXT')
         if errorText:
             icStateNum += 1
             continue
-        
-        #classTable = soup.find(id='win0divSSR_CLSRSLT_WRK_GROUPBOX2$0')
-        #className = classTable.find(id='win0divSSR_CLSRSLT_WRK_GROUPBOX2GP$0').text
-        #print(className)
+
+        # classTable = soup.find(id='win0divSSR_CLSRSLT_WRK_GROUPBOX2$0')
+        # className = classTable.find(id='win0divSSR_CLSRSLT_WRK_GROUPBOX2GP$0').text
+        # print(className)
         sectionGroups = {}
         courseId = ''
-        #for index, classRow in enumerate(classTable.find_all(id=lambda x: x and x.startswith('win0divSSR_CLSRSLT_WRK_GROUPBOX3$'))):
-        for index, classRow in enumerate(soup.find_all(id=lambda x: x and x.startswith('win0divSSR_CLSRSLT_WRK_GROUPBOX3$'))):
+        # for index, classRow in enumerate(classTable.find_all(id=lambda x: x and x.startswith('win0divSSR_CLSRSLT_WRK_GROUPBOX3$'))):
+        for index, classRow in enumerate(
+                soup.find_all(id=lambda x: x and x.startswith('win0divSSR_CLSRSLT_WRK_GROUPBOX3$'))):
             classRowIndex = classRow.get('id')[33:]
             classNumber = classRow.find(id=f'MTG_CLASS_NBR${classRowIndex}').text
             classSection = classRow.find(id=f'MTG_CLASSNAME${classRowIndex}').text
@@ -114,11 +128,18 @@ def get_valid_schedules():
                     'ICAction': f'MTG_CLASS_NBR${classRowIndex}',
                 }
 
-                response = requests.post('https://cmsweb.fresnostate.edu/psc/CFRPRD/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL', cookies=cookies, data=data)
+                response = requests.post(
+                    'https://cmsweb.fresnostate.edu/psc/CFRPRD/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL',
+                    cookies=cookies, data=data)
                 soup = BeautifulSoup(response.text, 'html5lib')
                 courseId = soup.find(id='SSR_CLS_DTL_WRK_CRSE_ID').text
             sectionTypeId = classSection.split('-')[1]
-            section = { 'id': classNumber, 'number': classSection.split('-')[0], 'type': sectionTypeId, 'days': re.findall('[A-Z][^A-Z]*', classDayTime.split()[0]), 'startTime': datetime.datetime.strptime(classDayTime.split()[1], '%I:%M%p').time().isoformat(), 'endTime': datetime.datetime.strptime(classDayTime.split()[3], '%I:%M%p').time().isoformat(), 'room': classRoom, 'instructor': classInstructor.splitlines()[0], 'courseId': courseId, 'courseName': className }
+            section = {'id': classNumber, 'number': classSection.split('-')[0], 'type': sectionTypeId,
+                       'days': re.findall('[A-Z][^A-Z]*', classDayTime.split()[0]),
+                       'startTime': datetime.datetime.strptime(classDayTime.split()[1], '%I:%M%p').time().isoformat(),
+                       'endTime': datetime.datetime.strptime(classDayTime.split()[3], '%I:%M%p').time().isoformat(),
+                       'room': classRoom, 'instructor': classInstructor.splitlines()[0], 'courseId': courseId,
+                       'courseName': className}
             if sectionGroups.get(sectionTypeId) is None:
                 sectionGroups[sectionTypeId] = [section]
             else:
@@ -127,10 +148,11 @@ def get_valid_schedules():
     schedules = generate_schedules(classes, 0)
     return schedules
 
+
 def generate_schedules(classes, level):
     if level >= len(classes):
         return [[]]
-    
+
     schedules = []
     for section in classes[level]:
         for schedule in generate_schedules(classes, level + 1):
@@ -142,13 +164,16 @@ def generate_schedules(classes, level):
                 schedules.append(schedule)
     return schedules
 
+
 def is_valid_schedule(schedule):
     for class1 in schedule:
         for class2 in schedule:
             if class1 == class2:
                 continue
-            
+
             if not set(class1['days']).isdisjoint(class2['days']):
-                if (class2['startTime'] >= class1['startTime'] and class2['startTime'] <= class1['endTime']) or (class2['endTime'] >= class1['startTime'] and class2['endTime'] <= class1['endTime']) or (class2['startTime'] < class1['startTime'] and class2['endTime'] > class1['endTime']):
+                if (class2['startTime'] >= class1['startTime'] and class2['startTime'] <= class1['endTime']) or (
+                        class2['endTime'] >= class1['startTime'] and class2['endTime'] <= class1['endTime']) or (
+                        class2['startTime'] < class1['startTime'] and class2['endTime'] > class1['endTime']):
                     return False
     return True
