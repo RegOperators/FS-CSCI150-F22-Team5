8bd4d83f9cec8eb0a3423b2cc38eff47a664ee05
Luis Gutierrez <byfractl@gmail.com>
2022-11-09 10:02:10 -0800

    Add page routing, move the schedule results to a separate page, and add a term selection step

--- app/src/App.js
+++ app/src/App.js
@@ -1,199 +1,28 @@
-import { useState, useEffect } from 'react'
-import logo from './logo.svg';
-import './App.css';
-import CoursesStep from './components/CoursesStep'
-import BusyPeriodsStep from './components/BusyPeriodsStep'
-import SchedulesStep from './components/SchedulesStep'
-import { motion, AnimatePresence } from 'framer-motion'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './components/RootLayout'
+import ScheduleGenerator from './pages/ScheduleGenerator'
+import ScheduleGeneratorResults from './pages/ScheduleGeneratorResults'
 
 function App() {
-  const [theme, setTheme] = useState(localStorage.theme || 'system')
-
-  useEffect(() => {
-    if (theme === 'dark' || (theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout />,
+      children: [
+        {
+          path: 'schedule-generator',
+          element: <ScheduleGenerator />
+        },
+        {
+          path: 'schedule-generator/results',
+          element: <ScheduleGeneratorResults />
+        }
+      ]
     }
-  }, [theme])
+  ])
   
-  const systemThemeIcon = (
-    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="currentColor" viewBox="0 0 512 512">
-      <title>Contrast</title>
-      <path d="M256 32A224 224 0 0097.61 414.39 224 224 0 10414.39 97.61 222.53 222.53 0 00256 32zM64 256c0-105.87 86.13-192 192-192v384c-105.87 0-192-86.13-192-192z"/>
-    </svg>
-  )
-
-  const lightThemeIcon = (
-    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="currentColor" viewBox="0 0 512 512">
-      <title>Sunny</title>
-      <path d="M256 118a22 22 0 01-22-22V48a22 22 0 0144 0v48a22 22 0 01-22 22zM256 486a22 22 0 01-22-22v-48a22 22 0 0144 0v48a22 22 0 01-22 22zM369.14 164.86a22 22 0 01-15.56-37.55l33.94-33.94a22 22 0 0131.11 31.11l-33.94 33.94a21.93 21.93 0 01-15.55 6.44zM108.92 425.08a22 22 0 01-15.55-37.56l33.94-33.94a22 22 0 1131.11 31.11l-33.94 33.94a21.94 21.94 0 01-15.56 6.45zM464 278h-48a22 22 0 010-44h48a22 22 0 010 44zM96 278H48a22 22 0 010-44h48a22 22 0 010 44zM403.08 425.08a21.94 21.94 0 01-15.56-6.45l-33.94-33.94a22 22 0 0131.11-31.11l33.94 33.94a22 22 0 01-15.55 37.56zM142.86 164.86a21.89 21.89 0 01-15.55-6.44l-33.94-33.94a22 22 0 0131.11-31.11l33.94 33.94a22 22 0 01-15.56 37.55zM256 358a102 102 0 11102-102 102.12 102.12 0 01-102 102z"/>
-    </svg>
-  )
-
-  const darkThemeIcon = (
-    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="currentColor" viewBox="0 0 512 512">
-      <title>Moon</title>
-      <path d="M264 480A232 232 0 0132 248c0-94 54-178.28 137.61-214.67a16 16 0 0121.06 21.06C181.07 76.43 176 104.66 176 136c0 110.28 89.72 200 200 200 31.34 0 59.57-5.07 81.61-14.67a16 16 0 0121.06 21.06C442.28 426 358 480 264 480z"/>
-    </svg>
-  )
-  
-  const options = [
-    { value: 'system', component: systemThemeIcon },
-    { value: 'light', component: lightThemeIcon },
-    { value: 'dark', component: darkThemeIcon }
-  ]
-
-  const [optionIndex, setOptionIndex] = useState(options.findIndex((option) => option.value === theme))
-  
-  const switchTheme = () => {
-    const newOptionIndex = (optionIndex + 1) % options.length
-    setOptionIndex(newOptionIndex)
-    setTheme(options[newOptionIndex].value)
-    localStorage.theme = options[newOptionIndex].value
-  }
-  
-  const [formData, setFormData] = useState({
-    courses: [],
-    busyPeriods: []
-  })
-
-  const steps = [
-    { name: 'Select Courses', component: <CoursesStep formData={formData} setFormData={setFormData} /> },
-    { name: 'Add Busy Periods', component: <BusyPeriodsStep formData={formData} setFormData={setFormData} /> },
-    { name: 'Compare Schedules', component: <SchedulesStep formData={formData} setFormData={setFormData} /> }
-  ]
-
-  const [currentStepIndex, setCurrentStepIndex] = useState(0)
-
-  const goForward = () => {
-    if (currentStepIndex < steps.length - 1) {
-      setCurrentStepIndex(currentStepIndex + 1)
-    }
-  }
-
-  const goBack = () => {
-    if (currentStepIndex > 0) {
-      setCurrentStepIndex(currentStepIndex - 1)
-    }
-  }
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  const tableResults = () => {
-    var res = ""
-    for(let i = 0; i < potentialScheduleRes.length; i++){
-      res += "<br/><br/><table> <tr><td></td><th>Monday</th><th>Tuesday</th><th>Wednesday</th><th>Thursday</th><th>Friday</th></tr>" + scheduleRow(potentialScheduleRes[i]) + "</table>"
-    }
-    document.getElementById("resultTables").innerHTML = res
-  }
-
-  const scheduleRow = (classes) => {
-    var x = classes
-    x.sort(compareFn)
-    console.log(x)
-    var res1 = ""
-    for(let i = 0; i < x.length; i++){
-      res1 += "<tr><td></td>"
-      if(x[i].days.includes("Mo")){
-        res1 += "<td>" + x[i].courseName + " " + x[i].type +"<br/>" + x[i].id
-        res1 += "<br/> Time: " + getTime(x[i].startTime) + " - " + getTime(x[i].endTime)
-        res1 += "<br/> Instructor: " + x[i].instructor
-        res1 += "</td>"
-      }else {res1 += "<td></td>"}
-
-      if(x[i].days.includes("Tu")){
-        res1 += "<td>" + x[i].courseName + " " + x[i].type +"<br/>" + x[i].id
-        res1 += "<br/> Time: " + getTime(x[i].startTime) + " - " + getTime(x[i].endTime)
-        res1 += "<br/> Instructor: " + x[i].instructor
-        res1 += "</td>"
-      }else {res1 += "<td></td>"}
-
-      if(x[i].days.includes("We")){
-        res1 += "<td>" + x[i].courseName + " " + x[i].type +"<br/>" + x[i].id
-        res1 += "<br/> Time: " + getTime(x[i].startTime) + " - " + getTime(x[i].endTime)
-        res1 += "<br/> Instructor: " + x[i].instructor
-        res1 += "</td>"
-      }else {res1 += "<td></td>"}
-
-      if(x[i].days.includes("Th")){
-        res1 += "<td>" + x[i].courseName + " " + x[i].type +"<br/>" + x[i].id
-        res1 += "<br/> Time: " + getTime(x[i].startTime) + " - " + getTime(x[i].endTime)
-        res1 += "<br/> Instructor: " + x[i].instructor
-        res1 += "</td>"
-      }else {res1 += "<td></td>"}
-
-      if(x[i].days.includes("Fr")){
-        res1 += "<td>" + x[i].courseName + " " + x[i].type +"<br/>" + x[i].id
-        res1 += "<br/> Time: " + getTime(x[i].startTime) + " - " + getTime(x[i].endTime)
-        res1 += "<br/> Instructor: " + x[i].instructor
-        res1 += "</td>"
-      }else {res1 += "<td></td>"}
-
-      res1 += "<tr>"
-    }
-
-    return res1
-
-  }
-
-  const getTime = (timeGiven) => {
-    let myArray = timeGiven.split(':');
-
-    let x = parseFloat(myArray[0]);
-    if(x > 12)
-      x = x % 12
-    
-    return (x.toString() + ":" + myArray[1])
-  }
-
-  const compareFn = (firstItem, secondItem) => {
-    const myArray = firstItem.startTime.split(':')
-    myArray[1] = "0." + myArray[1]
-    var a = parseFloat(myArray[0]) + parseFloat(myArray[1])
-     
-    const myArray1 = secondItem.startTime.split(':')
-    myArray1[1] = "0." + myArray1[1]
-    var b = parseFloat(myArray1[0]) + parseFloat(myArray1[1])
-
-    if(a < b)
-      return -1
-    else if(a > b)
-      return 1
-    else 
-      return 0
-  }
-
-  
-
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   return (
-    <div className="dark:bg-[#0d1117] dark:text-white font-['Manrope'] min-h-screen">
-      <header className="flex justify-between items-center px-6 sm:px-12 py-6 sm:py-8">
-        <h1 className="text-xl sm:text-2xl font-semibold">RegOp</h1>
-        <button onClick={switchTheme}>{options[optionIndex].component}</button>
-      </header>
-      <div>
-        <div className="container mx-auto px-6 sm:px-12 py-24 sm:py-32 2xl:py-48 grid grid-cols-1 sm:grid-cols-10 gap-16">
-          <div className="hidden sm:block col-span-3 sticky top-32 h-min">
-            <ol>
-              {steps.map((step, index) => (
-                <li className={`flex items-center mb-16 last:mb-0 ${index !== currentStepIndex ? 'text-gray-500 dark:text-gray-400' : ''}`} onClick={() => setCurrentStepIndex(index)} key={index}><div className="bg-gray-100 dark:bg-[#161b22] w-8 h-8 rounded-md flex justify-center items-center mr-4">{index + 1}</div>{step.name}</li>
-              ))}
-            </ol>
-          </div>
-          <AnimatePresence initial={false} mode="wait">
-            <motion.div className="sm:col-span-7" key={currentStepIndex} initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -50 }} transition={{ type: 'tween' }}>{steps[currentStepIndex].component}</motion.div>
-          </AnimatePresence>
-        </div>
-        <div className="fixed bottom-0 w-full z-10 bg-white dark:bg-[#0d1117] py-6">
-          <div className="container mx-auto px-6 sm:px-12 flex justify-end">
-            <button className="bg-gray-100 dark:bg-[#161b22] font-semibold px-4 py-2.5 rounded-md w-full sm:w-auto mr-4" onClick={goBack}>Back</button>
-            <button className="bg-indigo-500 text-white font-semibold px-4 py-2.5 rounded-md w-full sm:w-auto" onClick={goForward}>Continue</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ app/src/pages/ScheduleGeneratorResults.js
@@ -0,0 +1,123 @@
+import { useState, useEffect, Fragment } from 'react'
+import { useLocation } from 'react-router-dom'
+import { motion } from 'framer-motion'
+
+const ScheduleGeneratorResults = () => {
+  const location = useLocation()
+  const formData = location.state
+  
+  const [schedules, setSchedules] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  
+  useEffect(() => {
+    fetch('http://localhost:4000?' + new URLSearchParams(formData.courses.map((course) => ['classes', `${course.SUBJECT_ID} ${course.CATALOG_NBR}`])).toString())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not OK')
+      }
+      return response.json()
+    })
+    .then((data) => {
+      setSchedules(data)
+      setError(null)
+    })
+    .catch((error) => {
+      setError(error.message)
+      setSchedules(null)
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  const [offset, setOffset] = useState(Math.floor(Math.random() * 360))
+  
+  const calculateColor = (index) => {
+    return `hsl(${(137.5 * index) + offset}, 84%, 67%)`
+  }
+  
+  const groupSectionsByDay = (sections) => {
+    const groups = new Map()
+    for (const dayId of ['Mo', 'Tu', 'We', 'Th', 'Fr']) {
+      groups.set(dayId, [])
+      for (const section of sections) {
+        if (section.days.includes(dayId)) {
+          groups.get(dayId).push(section)
+        }
+      }
+    }
+    return groups
+  }
+  
+  const militaryToRegularTime = (time) => {
+    return new Date(`1970-01-01T${time}`).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })
+  }
+  
+  const startTime = 8
+  const endTime = 19
+  
+  return (
+    <div className="container mx-auto px-6 sm:px-12 py-24 sm:py-32 2xl:py-48">
+      <h1 className="text-3xl xl:text-5xl 2xl:text-6xl font-extrabold mb-8">{schedules.length} Possible Schedules</h1>
+      {schedules.length > 0 ? (
+        <ul>
+          {schedules.map((schedule, index) => (
+            <li className="grid grid-cols-1 sm:grid-cols-10 gap-16 mb-8 last:mb-0" key={index}>
+              <div className="sm:col-span-4 sm:sticky sm:top-16 sm:h-min">
+                <h2 className="text-xl font-semibold mb-8">{index + 1}</h2>
+                <ul>
+                  {schedule.map((section, index) => (
+                    <li key={index}>
+                      <div>{section.courseName}</div>
+                      <div>{militaryToRegularTime(section.startTime)} - {militaryToRegularTime(section.endTime)}</div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="sm:col-span-6 grid grid-cols-[auto_repeat(5,_1fr)] overflow-x-auto" style={{ gridTemplateRows: `auto repeat(${endTime - startTime}, 1fr)` }}>
+                <div className="bg-white dark:bg-[#0d1117] sticky left-0 z-10" style={{ gridArea: '1 / 1 / 2 / 2' }}></div>
+                <div className="text-center pb-4" style={{ gridArea: '1 / 2 / 2 / 3' }}>Mo</div>
+                <div className="text-center pb-4" style={{ gridArea: '1 / 3 / 2 / 4' }}>Tu</div>
+                <div className="text-center pb-4" style={{ gridArea: '1 / 4 / 2 / 5' }}>We</div>
+                <div className="text-center pb-4" style={{ gridArea: '1 / 5 / 2 / 6' }}>Th</div>
+                <div className="text-center pb-4" style={{ gridArea: '1 / 6 / 2 / 7' }}>Fr</div>
+                {Array.from({ length: endTime - startTime }, (v, i) => militaryToRegularTime(`${(startTime + i).toString().padStart(2, '0')}:00`)).map((time, index) => (
+                  <div className="bg-white dark:bg-[#0d1117] sticky left-0 z-10 min-h-[8rem] pr-4" style={{ gridArea: `${index + 2} / 1 / ${index + 3} / 2` }} key={index}>
+                    <div className="text-xs -translate-y-1/2">{time}</div>
+                  </div>
+                ))}
+                {Array.from({ length: 5 * (endTime - startTime) }, (v, i) => (
+                  <div className="border-t border-gray-200 dark:border-[#30363d]" style={{ gridArea: `${Math.floor(i / 5) + 2} / ${(i % 5) + 2} / ${Math.floor(i / 5) + 3} / ${(i % 5) + 3}` }} key={i}></div>
+                ))}
+                {Array.from(groupSectionsByDay(schedule)).map(([key, value], sectionGroupIndex) => (
+                  <Fragment key={sectionGroupIndex}>
+                    {value.map((section, index) => (
+                      <div className="bg-gray-100 dark:bg-[#161b22] border-l-4 p-1 rounded-md text-center relative m-0.5" style={{ borderColor: calculateColor(formData.courses.findIndex((course) => course.CRSE_ID === section.courseId)), gridArea: `${(parseInt(section.startTime.split(':')[0]) - startTime) + 2} / ${sectionGroupIndex + 2} / ${(parseInt(section.endTime.split(':')[0]) - startTime) + 3} / ${sectionGroupIndex + 3}`, top: `${(((parseInt(section.startTime.split(':')[0]) + (parseInt(section.startTime.split(':')[1]) / 60)) - parseInt(section.startTime.split(':')[0])) / ((parseInt(section.endTime.split(':')[0]) + 1) - parseInt(section.startTime.split(':')[0]))) * 100}%`, height: `${(((parseInt(section.endTime.split(':')[0]) + (parseInt(section.endTime.split(':')[1]) / 60)) - (parseInt(section.startTime.split(':')[0]) + (parseInt(section.startTime.split(':')[1]) / 60))) / ((parseInt(section.endTime.split(':')[0]) + 1) - parseInt(section.startTime.split(':')[0]))) * 100}%` }} key={index}>
+                        <div className="text-sm font-semibold">{section.courseName}</div>
+                        <div className="text-xs">{militaryToRegularTime(section.startTime)} - {militaryToRegularTime(section.endTime)}</div>
+                        <div className="text-xs">{section.room}</div>
+                      </div>
+                    ))}
+                  </Fragment>
+                ))}
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>No courses have been selected.</div>
+      )}
+      {loading && (
+        <div className="fixed inset-0 bg-white dark:bg-[#0d1117] flex flex-col justify-center items-center">
+          <motion.div className="flex mb-8" variants={{ start: { transition: { staggerChildren: 0.2 } }, end: { transition: { staggerChildren: 0.2 } } }} initial="start" animate="end">
+            <motion.div className="bg-black dark:bg-white w-4 h-4 rounded-full mr-4" variants={{ start: { y: '0%' }, end: { y: '100%' } }} transition={{ duration: 0.4, repeat: Infinity, repeatType: 'reverse' }} />
+            <motion.div className="bg-black dark:bg-white w-4 h-4 rounded-full mr-4" variants={{ start: { y: '0%' }, end: { y: '100%' } }} transition={{ duration: 0.4, repeat: Infinity, repeatType: 'reverse' }} />
+            <motion.div className="bg-black dark:bg-white w-4 h-4 rounded-full" variants={{ start: { y: '0%' }, end: { y: '100%' } }} transition={{ duration: 0.4, repeat: Infinity, repeatType: 'reverse' }} />
+          </motion.div>
+          <div>Generating...</div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ScheduleGeneratorResults
